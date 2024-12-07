59f895d39b810e80f74beaa964e96dcae3f77be9
WooJHo <tbwjh3519@gmail.com>
2024-12-07 13:59:03 +0900

    creaft complete

--- frontend/upload_image/src/components/ImageUpload.jsx
+++ frontend/upload_image/src/components/ImageUpload.jsx
@@ -30,8 +30,8 @@ const UploadBox = () => {
   const processFile = (file) => {
     const { size: byteSize, type } = file;
 
-    if (byteSize > 3 * 1024 * 1024) {
-      alert("File size must be less than 3MB.");
+    if (byteSize > 10 * 1024 * 1024) {
+      alert("File size must be less than 10MB.");
       return;
     }
 
@@ -128,7 +128,7 @@ const UploadBox = () => {
             <>
                 <Logo />
                 <p className="preview_msg">클릭 혹은 파일을 이곳에 드롭하세요.</p>
-                <p className="preview_desc">파일당 최대 3MB</p>
+                <p className="preview_desc">파일당 최대 10MB</p>
             </>
             )}
             </label>
